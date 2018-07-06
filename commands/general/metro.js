f0cf7e9e52f296b89f13e7e66ae748e1a1adf4c6
Larry <luarblx@gmail.com>
2018-07-06 04:29:55 -0400

    x

--- commands/general/metro.js
+++ commands/general/metro.js
@@ -49,7 +49,7 @@ module.exports = class MetroCommand extends commando.Command {
                     var lType = t.find('.text-label').text()
                     var lValue = t.find('.text-lead').text()
 
-                    console.log(lType + " :: " + lValue)
+                    str += lType + " " + lValue + "\n";
                 });
 
             })
