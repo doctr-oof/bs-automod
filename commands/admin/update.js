1e68b9d756ec03206c960e0b4ce78bad56993715
doctr-oof <40879886+doctr-oof@users.noreply.github.com>
2018-07-06 01:37:31 -0400

    update cmd

---
+++ commands/admin/update.js
@@ -0,0 +1,28 @@
+const commando = require("discord.js-commando");
+const config = require("../../config.json");
+const { exec } = require("child_process");
+
+module.exports = class UpdateCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: "update",
+            group: "admin",
+            memberName: "update",
+            description: "Forces the bot to pull from git and update.",
+            examples: [ "update" ]
+        });
+    }
+
+    hasPermission(message) {
+        return this.client.isOwner(message.author) || "You don't have permission to use this command!";
+    }
+
+    async run(message) {
+        message.guild.channels.get(config.logging_channel).send(`[${new Date().toISOString().replace(/T/, " ").replace(/\..+/, "")}] **BOT UPDATE** - I'm updating.... hopefully this doesn't break. BRB!`);
+
+        exec("../../run.bat");
+        process.exit();
+
+        message.delete();
+    }
+}
